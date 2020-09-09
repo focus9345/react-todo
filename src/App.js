fa6a96504dabdf33d0350e08137841d0634df5bf
focus9345 <connor.joshua@gmail.com>
2020-09-09 15:39:14 -0400

    router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The React ToDo App</h1>
       </header>
+      <main className="App-main">
+          <section className="col"><h2>Organization</h2>
+          <ul>
+            <li>Primary</li>
+            <li></li>
+          </ul>
+          </section>
+          <section className="col"><h2>Tasks</h2></section>
+          <section className="col"><h2>Add New Task</h2></section>
+      </main>
+      <footer className="App-footer">
+        <p>Todays Date is: <span className="displayDate"></span></p>
+      </footer>
     </div>
   );
 }
